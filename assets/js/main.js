4d388cd758b90314deb9c0e437c86ea51f98a2da
J-Sanabria <70994415+J-Sanabria@users.noreply.github.com>
2025-11-16 22:09:00 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('./sw.js');
 }
 
 // ===== BLOODSTEEL PRELOADER (espera a precache + imágenes) =====
